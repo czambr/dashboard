8a5eb51a4eb20463db619c2a2bf6cc28e0bd812d
czambr <cmzambra@espol.edu.ec>
2023-02-07 21:40:24 -0500

    dashboard: debugging y DOM

---
+++ assets/js/update.js
@@ -0,0 +1,31 @@
+runScripts = () => {
+    let chart_bars = document.getElementById("chart-bars");
+    console.log(chart_bars);
+
+    let icons = document.getElementsByTagName("i");
+    console.log(icons);
+
+    let cards = document.getElementsByClassName("card");
+    console.log(cards);
+};
+
+
+modifyText = () => {
+    let listOfP = document.getElementsByTagName("p");
+    let elementP = listOfP[3];
+    elementP.innerHTML = 'Dinero Actual';
+
+    let listOfH4 = document.getElementsByTagName ("h4");
+    let elementH4 = listOfH4[0];
+    elementH4.innerHTML = '$301k'
+
+};
+
+
+
+// Llamado a las funciones  
+
+
+runScripts ();
+
+modifyText();
